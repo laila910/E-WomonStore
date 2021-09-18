d403951719150fa042aa02e34a449f355ccc7d47
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-09-18 02:54:50 +0200

    end of test

---
+++ routes/customer.routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const customerController = require('../controller/customer.controller')
+
+
+
+
+
+
+
+module.exports = router
--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 require("dotenv").config()
 require('../db/connection')
+
 const express = require('express')
+const customerRoutes = require('../routes/customer.routes')
+
 const app = express()
 
 
 
 app.use(express.json())
-app.get('/test', (req, res) => {
-    res.send('test')
-})
+app.use(customerRoutes)
+
 module.exports = app
