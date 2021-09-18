af7f613ab8d780a34a4c90ce907ce8215fae3b39
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-09-18 02:29:14 +0200

    start to create Apis

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const mongoose = require("mongoose")
-mongoose.connect('mongodb://127.0.0.1:27017/estore')
-
-const Customer = new mongoose.model('Customer', {
-    customerFirstName: {
-        type: String
-    },
-    customerLastName: {
-        type: String
-    }
-})
-
 // customerFirstName
 // customerLastName
 // customerEmail
@@ -32,5 +20,3 @@ const Customer = new mongoose.model('Customer', {
 // del customer
 // get all customers
 // show single customer
-myData = new Customer({ customerFirstName: "laila", customerLastName: "Ibrahim" })
-myData.save().then(() => console.log(myData)).catch(e => console.log(e))
