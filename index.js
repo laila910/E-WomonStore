3633e7970cc00761165366b2c9157dc09c018a6d
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-09-18 04:34:01 +0200

    addCustomer

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// customerFirstName
-// customerLastName
-// customerEmail
-// customerMobileNo
-// customerPassword
-// customerAddress
-// customerCountry
-// customerState
-// customerZipCode
-// customerCreditCard: enum["visa","discover","amex","mastercard"]
-// customerCreditCardTypeId
-// customerExpMonth
-// customerExpYr
-// customerCVC :"Card Verification Code"
-// customerDateRegistered : Date with default new Date()
-
-
 // add customer
 // edit customer
 // del customer
--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
 const customerController = require('../controller/customer.controller')
 
-
+const customer = require('../models/customer.model')
+router.post('/addCustomer', (req, res) => {
+    const myData = new customer(req.body)
+    myData.save().then(() => res.send(myData))
+        .catch(e => res.send(e))
+})
 
 
 
