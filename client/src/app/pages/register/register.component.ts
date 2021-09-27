5a0de807a45726084cc648d62e2759c1ffc6a0d1
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-09-27 03:37:19 +0200

    register

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -7,29 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  userData = {
-    name: "",
-    email: "",
-    password: "",
-    mobileNo: "",
-    userType: "",
-    accountStatus: false,
-    supplierCompanyName: "",
-    supplierCompanyFax: "",
-    supplierCompanyURL: "",
-    customerCreditCard: "",
-    customerCreditCardTypeId: "",
-    customerExpMonth: "",
-    customerExpYr: "",
-    customerCVC: ""
+  userType: any
 
-  }
+  userData: any = {}
   constructor(private _userService: UserService) { }
 
   ngOnInit(): void {
   }
   onRegister(data: any) {
-    // console.log(data.value)
+    if (this.userType == 2) {
+      this.userData = {
+        name: "",
+        email: "",
+        password: "",
+        mobileNo: "",
+        userType: this.userType,
+        accountStatus: false,
+        supplierCompanyName: "",
+        supplierCompanyFax: "",
+        supplierCompanyURL: ""
+      }
+    } else if (this.userType == 3) {
+      this.userData = {
+        name: "",
+        email: "",
+        password: "",
+        mobileNo: "",
+        userType: this.userType,
+        accountStatus: false,
+        customerCreditCard: "",
+        customerCreditCardTypeId: "",
+        customerExpMonth: "",
+        customerExpYr: "",
+        customerCVC: ""
+      }
+    } else if (this.userType == 1) {
+      this.userData = {
+        name: "",
+        email: "",
+        password: "",
+        mobileNo: "",
+        userType: this.userType,
+        accountStatus: true
+      }
+    }
+    console.log(data.value)
     if (data.valid) {
       console.log(this.userData)
       this._userService.registerUser(this.userData).subscribe(data => {
--- client/src/app/shared/searchbar/searchbar.component.ts
+++ client/src/app/shared/searchbar/searchbar.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./searchbar.component.css']
 })
 export class SearchbarComponent implements OnInit {
-
+  // searchProduct: string = ""
   constructor() { }
-
+  // arrayOfProducts: [] = []
   ngOnInit(): void {
   }
-
+  // result: any[] = this.arrayOfProducts
+  // searchP() {
+  //   this.result = this.arrayOfProducts.filter(item => {
+  //     // harg3 ba3den l hena
+  //     // return item.name.includes(this.searchProduct)
+  //   })
 }
+
+
